b20fb8c20e62715ba33f48d4d848a06c24ac2ba4
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-29 21:25:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function addToCart(item) {
     var oneItem = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
     return oneItem;
   } else if (cart.length === 2){
-    var twoItems = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${Object.keys(cart[1])} at $${cart[1].itemPrice}.`
+    var twoItems = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice} and ${cart[0].itemName} at $${cart[1].itemPrice}.`
     console.log(twoItems)
   } else {
     var cartData = []
